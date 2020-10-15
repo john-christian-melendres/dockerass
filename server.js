cbae61eb3a06106b137828699c99e35974f86283
john <john.melendres@mllrdev.com>
2020-10-16 01:26:38 +0800

    dockerizer

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+
+app.use(express.static(__dirname + '/dist/dockerAss'));
+
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname+'/dist/dockerAss/index.html'));
+});
+
+
+app.listen(3000, ()=> {
+    console.log('Server Running')
+});
